1eee474c4f37cc7fedbc027353644a09393ad213
Jonayet <jlavin@caltech.edu>
2024-02-28 01:04:36 -0800

    DDL change and added some data

---
+++ load-beyblade.sql
@@ -0,0 +1,35 @@
+-- Instructions:
+-- This script will load the 4 CSV files you broke down from playlist_data.csv
+-- into the tables you created in setup-spotify.sql.
+-- Intended for use with the command-line MySQL, otherwise unnecessary for
+-- phpMyAdmin (just import each CSV file in the GUI).
+
+-- Make sure this file is in the same directory as your 5 CSV files and
+-- setup.sql. Then run the following in the mysql> prompt (assuming
+-- you have a spotifydb created with CREATE DATABASE spotifydb;):
+-- USE DATABASE beybladedb; 
+-- source setup.sql; (make sure no warnings appear)
+-- source load-beyblade.sql; (make sure there are 0 skipped/warnings)
+
+-- Load the data for users table
+LOAD DATA LOCAL INFILE 'users.csv' INTO TABLE artists
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+-- Load the data for parts table
+LOAD DATA LOCAL INFILE 'parts.csv' INTO TABLE albums
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS;
+
+-- Load the data for beyblades table
+LOAD DATA LOCAL INFILE 'beyblades.csv' INTO TABLE playlists
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+-- Load the data for userbeyblades table
+LOAD DATA LOCAL INFILE 'userbeyblades.csv' INTO TABLE tracks
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+-- Load the data for battles table
+LOAD DATA LOCAL INFILE 'battles.csv' INTO TABLE tracks
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+
+
--- setup.sql
+++ setup.sql
@@ -1,36 +1,54 @@
+-- Remove existing tables to prevent errors on creation
 DROP TABLE IF EXISTS battles;
 DROP TABLE IF EXISTS userbeyblades;
 DROP TABLE IF EXISTS beyblades;
 DROP TABLE IF EXISTS parts;
 DROP TABLE IF EXISTS users;
 
+-- Table for storing user informaiton
 CREATE TABLE users (
+    -- Unique identifier for each user
     user_ID INT AUTO_INCREMENT PRIMARY KEY,
+    -- Username of user
     username VARCHAR(250) NOT NULL,
+    -- Email address of user
     email VARCHAR(250) NOT NULL UNIQUE,
+    -- Flag to indicate whether the user has administrative priveleges
     is_admin BOOLEAN NOT NULL,
+    -- The date and time the user joined
     date_joined DATETIME NOT NULL
 );
 
+-- Table for storing compilation of beyblade parts
 CREATE TABLE parts (
-    part_ID VARCHAR(50) PRIMARY KEY,
-    part_name VARCHAR(250) NOT NULL,
+    -- Unique identifier for each part
+    part_ID VARCHAR(20) PRIMARY KEY,
+    -- Type of the part 
     part_type ENUM('Face Bolt', 'Energy Ring', 'Fusion Wheel', 'Spin Track', 'Performance Tip') NOT NULL,
-    weight DECIMAL(4,2), -- in grams
+    -- Weight of the part in grams
+    weight DECIMAL(4,2),
+    -- Brief description of the part
     description TEXT
 );
 
+-- Table for storing parts and aspects that make up a single beyblade
 CREATE TABLE beyblades (
-    beyblade_ID INT AUTO_INCREMENT PRIMARY KEY,
+    -- Unique identifier for each Beyblade
+    beyblade_ID VARCHAR(10) PRIMARY KEY,
+    -- Name of the Beyblade
     name VARCHAR(250) NOT NULL,
+    -- Type of the Beyblade
     type ENUM('Attack', 'Defense', 'Stamina', 'Balance') NOT NULL,
+    -- Flag to indicate whether the Beyblade is custom-made
     is_custom BOOLEAN NOT NULL,
+    -- Series the Beyblade is from
     series ENUM('Metal Fusion', 'Metal Masters', 'Metal Fury') NOT NULL,
-    face_bolt_ID VARCHAR(50),
-    energy_ring_ID VARCHAR(50),
-    fusion_wheel_ID VARCHAR(50),
-    spin_track_ID VARCHAR(50),
-    performance_tip_ID VARCHAR(50),
+    -- Foreign keys linking to the parts table
+    face_bolt_ID VARCHAR(20),
+    energy_ring_ID VARCHAR(20),
+    fusion_wheel_ID VARCHAR(20),
+    spin_track_ID VARCHAR(20),
+    performance_tip_ID VARCHAR(20),
     FOREIGN KEY (face_bolt_ID) REFERENCES parts(part_ID),
     FOREIGN KEY (energy_ring_ID) REFERENCES parts(part_ID),
     FOREIGN KEY (fusion_wheel_ID) REFERENCES parts(part_ID),
@@ -38,29 +56,41 @@ CREATE TABLE beyblades (
     FOREIGN KEY (performance_tip_ID) REFERENCES parts(part_ID)
 );
 
+-- Table for linking users with their Beyblades
 CREATE TABLE userbeyblades (
+    -- Unique identifier for user-owned Beyblades
     user_beyblade_ID INT AUTO_INCREMENT PRIMARY KEY,
+    -- References the owner of the Beyblade
     user_ID INT NOT NULL,
-    beyblade_ID INT NOT NULL,
+    -- Reference the Beyblade owned by the user
+    beyblade_ID VARCHAR(10) NOT NULL,
     FOREIGN KEY (user_ID) REFERENCES users(user_ID)
         ON DELETE CASCADE, -- if user is deleted in users table, their Beyblades are also deleted from userbeyblades table
     FOREIGN KEY (beyblade_ID) REFERENCES beyblades(beyblade_ID)
 );
 
+-- Table for storing battle results
 CREATE TABLE battles (
+    -- Unique identifier for each Battle
     battle_ID INT AUTO_INCREMENT PRIMARY KEY,
+    -- Name of the tournament the battle is from
     tournament_name VARCHAR(250),
+    -- Date of the battle
     date DATETIME NOT NULL,
+    -- Lovation of the battle
     location VARCHAR(250) NOT NULL,
+    -- References to the players and their Beyblades invovles in the battle
     player1_ID INT NOT NULL,
     player2_ID INT NOT NULL,
     player1_beyblade_ID INT NOT NULL,
     player2_beyblade_ID INT NOT NULL,
-    winner_ID INT, -- Can be NULL if the battle was a draw or the winner is not recorded
+    winner_ID INT, -- Can be NULL if battle was draw
+    -- delete player1's battle history if player1's user_ID is deleted in users table
     FOREIGN KEY (player1_ID) REFERENCES users(user_ID)
-        ON DELETE CASCADE, -- delete player1's battle history if player1's user_ID is deleted in users table
+        ON DELETE CASCADE, 
+    -- delete player2's battle history if player2's user_ID is deleted in users table
     FOREIGN KEY (player2_ID) REFERENCES users(user_ID)
-        ON DELETE CASCADE, -- delete player2's battle history if player2's user_ID is deleted in users table
+        ON DELETE CASCADE, 
     FOREIGN KEY (player1_beyblade_ID) REFERENCES userbeyblades(user_beyblade_ID),
     FOREIGN KEY (player2_beyblade_ID) REFERENCES userbeyblades(user_beyblade_ID),
     FOREIGN KEY (winner_ID) REFERENCES userbeyblades(user_beyblade_ID)
