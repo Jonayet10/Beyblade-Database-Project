46ff82571062a6d753b9f37519054ef021eb45e3
Jonayet <jlavin@caltech.edu>
2024-03-01 21:28:29 -0800

    app files

--- app-admin.py
+++ app-admin.py
@@ -28,12 +28,12 @@ def get_conn():
     try:
         conn = mysql.connector.connect(
           host='localhost',
-          user='appadmin',
+          user='jlavin',
           # Find port in MAMP or MySQL Workbench GUI or with
           # SHOW VARIABLES WHERE variable_name LIKE 'port';
           port='3306',  # this may change!
-          password='adminpw',
-          database='shelterdb' # replace this with your database name
+          password='jlavinpw',
+          database='beybladedb' # replace this with your database name
         )
         print('Successfully connected.')
         return conn
@@ -56,28 +56,30 @@ def get_conn():
 # ----------------------------------------------------------------------
 # Functions for Command-Line Options/Query Execution
 # ----------------------------------------------------------------------
-def example_query():
-    param1 = ''
-    cursor = conn.cursor()
-    # Remember to pass arguments as a tuple like so to prevent SQL
-    # injection.
-    sql = 'SELECT col1 FROM table WHERE col2 = \'%s\';' % (param1, )
-    try:
-        cursor.execute(sql)
-        # row = cursor.fetchone()
-        rows = cursor.fetchall()
-        for row in rows:
-            (col1val) = (row) # tuple unpacking!
-            # do stuff with row data
-    except mysql.connector.Error as err:
-        # If you're testing, it's helpful to see more details printed.
-        if DEBUG:
-            sys.stderr(err)
-            sys.exit(1)
-        else:
-            # TODO: Please actually replace this :) 
-            sys.stderr('An error occurred, give something useful for clients...')
 
+def add_beyblade(name, type, series, is_custom, face_bolt_id, energy_ring_id, fusion_wheel_id, spin_track_id, performance_tip_id):
+    cursor = conn.cursor()
+    sql = ("INSERT INTO beyblades (name, type, series, is_custom, face_bolt_id, energy_ring_id, fusion_wheel_id, spin_track_id, performance_tip_id) "
+           "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)")
+    data = (name, type, series, is_custom, face_bolt_id, energy_ring_id, fusion_wheel_id, spin_track_id, performance_tip_id)
+    try:
+        cursor.execute(sql, data)
+        conn.commit()
+        print(f"Added new Beyblade: {name}")
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+
+def add_battle(tournament_name, date, location, player1_id, player2_id, player1_beyblade_id, player2_beyblade_id, winner_id):
+    cursor = conn.cursor()
+    sql = ("INSERT INTO battles (tournament_name, date, location, player1_id, player2_id, player1_beyblade_id, player2_beyblade_id, winner_id) "
+           "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
+    data = (tournament_name, date, location, player1_id, player2_id, player1_beyblade_id, player2_beyblade_id, winner_id)
+    try:
+        cursor.execute(sql, data)
+        conn.commit()
+        print("New battle result added successfully.")
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
 
 
 # ----------------------------------------------------------------------
@@ -89,11 +91,66 @@ def example_query():
 # app-client.py vs. app-admin.py (in which case you don't need to
 # support any prompt functionality to conditionally login to the sql database)
 
+def is_admin(username):
+    """
+    Helper function to verify whether the user logging in is a BeyAdmin.
+    Checks the `is_admin` flag for the given username in the `users` table.
+    """
+    cursor = conn.cursor()
+    sql = "SELECT is_admin FROM users WHERE username = %s;"
+    try:
+        cursor.execute(sql, (username,))
+        result = cursor.fetchone()
+        # If the user exists and the is_admin flag is True, return True
+        if result and result[0]:
+            return True
+        else:
+            return False
+    except mysql.connector.Error as err:
+        print(f"Database error: {err}")
+        return False
+    finally:
+        cursor.close()
+
+def login():
+    """
+    This function prompts the login for an admin.
+    It checks the database to ensure that the username and password are correct.
+    """
+    cursor = conn.cursor()
+
+    print("\n------------------------------ BeyAdmin Login ------------------------------\n")
+
+    while True:
+        username = input("USERNAME: ").lower()
+        password = input("PASSWORD: ").lower()
+
+        while not is_admin(username):
+            print("\nIt appears that you are not a BeyAdmin. Please try again! \n")
+            username = input("USERNAME: ").lower()
+            password = input("PASSWORD: ").lower()
+
+        sql = "SELECT authenticate('%s', '%s');" % (username, password)
+
+        try:
+            cursor.execute(sql)
+            check_response = cursor.fetchone()
+
+            if check_response[0] == 1:
+                show_options()
+            else:
+                print("\nUsername or password is incorrect. Please try again :)\n")
+
+        except mysql.connector.Error as err:
+            if DEBUG:
+                sys.stderr(err)
+                sys.exit(1)
+            else:
+                sys.stderr("Error logging in.")
 
 # ----------------------------------------------------------------------
 # Command-Line Functionality
 # ----------------------------------------------------------------------
-# TODO: Please change these!
 def show_options():
     """
     Displays options users can choose in the application, such as
@@ -101,48 +158,49 @@ def show_options():
     sending a request to do <x>, etc.
     """
     print('What would you like to do? ')
-    print('  (TODO: provide command-line options)')
-    print('  (x) - something nifty to do')
-    print('  (x) - another nifty thing')
-    print('  (x) - yet another nifty thing')
-    print('  (x) - more nifty things!')
+    print('  (a) - Add a new Beyblade')
+    print('  (b) - Add a new battle result')
+    # Add more options as needed
     print('  (q) - quit')
     print()
     ans = input('Enter an option: ').lower()
+
     if ans == 'q':
         quit_ui()
-    elif ans == '':
-        pass
-
-
-# Another example of where we allow you to choose to support admin vs. 
-# client features  in the same program, or
-# separate the two as different app_client.py and app_admin.py programs 
-# using the same database.
-def show_admin_options():
-    """
-    Displays options specific for admins, such as adding new data <x>,
-    modifying <x> based on a given id, removing <x>, etc.
-    """
-    print('What would you like to do? ')
-    print('  (x) - something nifty for admins to do')
-    print('  (x) - another nifty thing')
-    print('  (x) - yet another nifty thing')
-    print('  (x) - more nifty things!')
-    print('  (q) - quit')
-    print()
-    ans = input('Enter an option: ').lower()
-    if ans == 'q':
+    elif ans == 'a':
+        # Prompt for Beyblade details and call add_beyblade function
+        name = input('Enter Beyblade name: ')
+        type = input('Enter Beyblade type (Attack, Defense, Stamina, Balance): ')
+        series = input('Enter Beyblade series (Metal Fusion, Metal Masters, Metal Fury): ')
+        is_custom = input('Is it custom? (True/False): ').lower() in ['true', '1', 't', 'y', 'yes']
+        face_bolt_id = input('Enter Face Bolt ID: ')
+        energy_ring_id = input('Enter Energy Ring ID: ')
+        fusion_wheel_id = input('Enter Fusion Wheel ID: ')
+        spin_track_id = input('Enter Spin Track ID: ')
+        performance_tip_id = input('Enter Performance Tip ID: ')
+        add_beyblade(name, type, series, is_custom, face_bolt_id, energy_ring_id, fusion_wheel_id, spin_track_id, performance_tip_id)
+    elif ans == 'b':
+        # Prompt for battle details and call a function to add battle result
+        tournament_name = input('Enter tournament name: ')
+        date = input('Enter date of the battle (YYYY-MM-DD HH:MM:SS): ')
+        location = input('Enter location: ')
+        player1_id = input('Enter Player 1 ID: ')
+        player2_id = input('Enter Player 2 ID: ')
+        player1_beyblade_id = input('Enter Player 1 Beyblade ID: ')
+        player2_beyblade_id = input('Enter Player 2 Beyblade ID: ')
+        winner_id = input('Enter Winner ID (leave blank if draw): ')
+        winner_id = winner_id if winner_id.strip() != '' else None
+        add_battle(tournament_name, date, location, player1_id, player2_id, player1_beyblade_id, player2_beyblade_id, winner_id)
+    elif ans == 'q':
         quit_ui()
-    elif ans == '':
-        pass
-
 
 def quit_ui():
     """
     Quits the program, printing a good bye message to the user.
     """
-    print('Good bye!')
+    print('\n----------------------------------------------------------------\n')
+    print('Thank you for keeping the Beyblade legacy ablaze. May the Beyblade spirit be with you. Goodbye!')
+    print('\n----------------------------------------------------------------\n')
     exit()
 
 
@@ -150,7 +208,7 @@ def main():
     """
     Main function for starting things up.
     """
-    show_options()
+    login()
 
 
 if __name__ == '__main__':
