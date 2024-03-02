9e4830eab2588be100c2161e3c12a8ccf87c1ed6
Jonayet <137201209+Jonayet10@users.noreply.github.com>
2024-03-01 20:36:10 -0800

    Update load-beyblade.sql

--- load-beyblade.sql
+++ load-beyblade.sql
@@ -12,23 +12,23 @@
 -- source load-beyblade.sql; (make sure there are 0 skipped/warnings)
 
 -- Load the data for users table
-LOAD DATA LOCAL INFILE 'users.csv' INTO TABLE artists
+LOAD DATA LOCAL INFILE 'users.csv' INTO TABLE users
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
 
 -- Load the data for parts table
-LOAD DATA LOCAL INFILE 'parts.csv' INTO TABLE albums
+LOAD DATA LOCAL INFILE 'parts.csv' INTO TABLE parts
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS;
 
 -- Load the data for beyblades table
-LOAD DATA LOCAL INFILE 'beyblades.csv' INTO TABLE playlists
+LOAD DATA LOCAL INFILE 'beyblades.csv' INTO TABLE beyblades
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
 
 -- Load the data for userbeyblades table
-LOAD DATA LOCAL INFILE 'userbeyblades.csv' INTO TABLE tracks
+LOAD DATA LOCAL INFILE 'userbeyblades.csv' INTO TABLE userbeyblades
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
 
 -- Load the data for battles table
-LOAD DATA LOCAL INFILE 'battles.csv' INTO TABLE tracks
+LOAD DATA LOCAL INFILE 'battles.csv' INTO TABLE battles
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
 
 
